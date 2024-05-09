b22ce27ec0e019f400f4ec523c5405ea349bc889
Tanner <tannercdolby@gmail.com>
2024-05-08 23:35:18 -0700

    sigh

--- src/app/feed/[id]/FeaturedPage.js
+++ src/app/feed/[id]/FeaturedPage.js
@@ -4,15 +4,15 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Link from "next/link";
 
-export default async function Page({ params={} }) {
-    // const {id} = params;
-    // const photo = await fetchPhoto(id);
+export default async function FeaturedPage({ params={} }) {
+    const {id} = params;
+    const photo = await fetchPhoto(id);
 
     return (
         <>
             <Header />
             <main className="flex justify-center items-center w-full my-6">
-                {/* <Image
+                <Image
                     src={photo.src.portrait}
                     alt={photo.alt}
                     width={640}
@@ -20,7 +20,7 @@ export default async function Page({ params={} }) {
                     className="rounded-sm max-h-[500px]"
 
                 />
-                <h1 className="max-w-[30ch] mt-4 ml-8">{photo.alt} by {photo.photographer} on <Link href={photo.url}>Pexels</Link></h1> */}
+                <h1 className="max-w-[30ch] mt-4 ml-8">{photo.alt} by {photo.photographer} on <Link href={photo.url}>Pexels</Link></h1>
             </main>
             <Footer />
         </>
