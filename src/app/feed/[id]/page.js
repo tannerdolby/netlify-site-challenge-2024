b5f7f40c5704dd4b8ff530c5b31f029d89e5ceaa
Tanner <tannercdolby@gmail.com>
2024-05-08 23:41:44 -0700

    let me cook

--- src/app/feed/[id]/page.js
+++ src/app/feed/[id]/page.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Link from "next/link";
 
-export default async function FeaturedPage({ params={} }) {
+export default async function Page({ params={} }) {
     const {id} = params;
     const photo = await fetchPhoto(id);
 
