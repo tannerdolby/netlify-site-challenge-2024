4444eb379a5eb2eca5b0fad5838a6d0f36533182
Tanner <tannercdolby@gmail.com>
2024-05-10 18:37:52 -0700

    Use '<button>' to ensure click on mobile devices and update responsiveness

--- src/app/components/ImageGallery.js
+++ src/app/components/ImageGallery.js
@@ -7,9 +7,23 @@ import "../styles/image-gallery.css";
 export default function ImageGallery({ data }) {
     return (
         <ul className="image-gallery">
-            {data?.map((image) => (
+            {data?.map((image, i) => (
                 <li key={image.id}>
-                    <Image
+                    <button 
+                        onClick={(e) => {
+                            const images = [...document.querySelectorAll('.featured-image')];
+                            animateImageWidth(images[i], image);
+                            shrinkAllButGrown(images[i], image);
+                        }}
+                        onKeyUp={(e) => {
+                            if (e.key === 'Enter') {
+                                const images = [...document.querySelectorAll('.featured-image')];
+                                animateImageWidth(images[i], image);
+                                shrinkAllButGrown(images[i], image);
+                            }
+                        }}
+                    >
+                        <Image
                         tabIndex={0}
                         src={image.src.original}
                         alt={image.alt}
@@ -18,18 +32,19 @@ export default function ImageGallery({ data }) {
                         title={image.title}
                         id={`image-${image.id}`}
                         className={`featured-image`}
-                        onClick={(e) => {
-                            animateImageWidth(e, image);
-                            shrinkAllButGrown(e, image);
-                        }}
-                        onKeyUp={(e) => {
-                            if (e.key === 'Enter') {
-                                animateImageWidth(e, image);
-                                shrinkAllButGrown(e, image);
-                            }
-                        }}
+                        // onClick={(e) => {
+                        //     animateImageWidth(e, image);
+                        //     shrinkAllButGrown(e, image);
+                        // }}
+                        // onKeyUp={(e) => {
+                        //     if (e.key === 'Enter') {
+                        //         animateImageWidth(e, image);
+                        //         shrinkAllButGrown(e, image);
+                        //     }
+                        // }}
                         priority
                     />
+                    </button>
                 </li>
             ))}
         </ul>
@@ -37,17 +52,17 @@ export default function ImageGallery({ data }) {
 }
 
 function animateImageWidth(e, image) {
-    if ([...e.target.classList].includes('grow-image')) {
-        e.target.classList.add('shrink-image');
-        e.target.classList.remove('grow-image');
+    if ([...e.classList].includes('grow-image')) {
+        e.classList.add('shrink-image');
+        e.classList.remove('grow-image');
         gsap.to(`#image-${image.id}`, {
             width: 130,
             ease: 'power4',
             duration: .5
         })
     } else {
-        e.target.classList.remove('shrink-image');
-        e.target.classList.add('grow-image');
+        e.classList.remove('shrink-image');
+        e.classList.add('grow-image');
         gsap.to(`#image-${image.id}`, {
             width: 425,
             ease: 'power4',
@@ -59,7 +74,7 @@ function animateImageWidth(e, image) {
 function shrinkAllButGrown(e) {
     const images = [...document.querySelectorAll('.featured-image')];
     for (const image of images) {
-        if (image != e.target && [...image.classList].includes('grow-image')) {
+        if (image != e && [...image.classList].includes('grow-image')) {
             image.classList.remove('grow-image');
             image.classList.add('shrink-image');
             gsap.to(`#${image.id}`, {
